b95cf5057794aa55767443a16b9104db3b2fcad7
AlonsoYuen19 <wwepg15@gmail.com>
2021-11-19 02:10:25 -0500

    UPDATE

--- src/main/java/com/dhome/appointmentmicroservice/events/AppointmentEdited.java
+++ src/main/java/com/dhome/appointmentmicroservice/events/AppointmentEdited.java
@@ -7,6 +7,7 @@ import java.time.Instant;
 @Value
 public class AppointmentEdited {
     private String appointmentId;
+    private String paymentId;
     private String date;
     private String description;
     private Instant occurredOn;
